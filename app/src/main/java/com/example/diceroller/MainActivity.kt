a735d699092952ce0ade59b381e3c30ecbe55b5d
Syed Izhaan S <izhaan9802@gmail.com>
2022-10-05 02:49:14 +0530

    Toast object created that enables the use of a function called makeText( <obj>, "text", Toast.LENGTH_SHORT/LONG), this displays a message to the user for a very short duration of time.
     TRYING SHORT

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { //this function is called by Android System when the app is first started
@@ -12,6 +13,22 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button) //The findViewById() method finds the Button in the layout.
                                                            // R.id.button is the resource ID for the Button, which is
                                                           // a unique identifier for it.
+
+        rollButton.setOnClickListener {                        //Instead of the parentheses following the method name, you will actually be using curly braces
+                                                               // following the method name. This is a special syntax for declaring a Lambda
+                                                           // within the curly braces, you put instructions for what should happen when the button is tapped.
+
+           val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            //a Toast, which is a brief message that appears to the user.
+            toast.show()
+            //tell the Toast to display itself by calling the show() method.
+            /*
+            You could combine the two lines in the click listener into a single line without a variable.
+            This is a common pattern you might find in other code.
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() <- without variable
+             */
+        }
+
     }
 }
 
